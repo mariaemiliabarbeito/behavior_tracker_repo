95b992924342ce7cb77caa099801022613fbcb23
matilde-urrestarazu <murrestarazuromero@udesa.edu.ar>
2026-04-16 15:57:00 -0300

    Update procesamiento_datos.py

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -6,4 +6,20 @@ Created on Tue Apr 14 10:39:04 2026
 @author: mariaemiliabarbeito
 """
 
-procesamiento_datos
+def filtrar_por_participante(datos, id_participante):
+    
+    '''
+    Que hace la funcion: La función obtiene y devuelve los datos de un participante especifico, ignorando los demás.
+    
+    Parametros:
+        datos (list): lista con la informacion de todos los participantes
+        id_participante (int): numero que identifica al participante cuyos datos se quiere obtener 
+    Retorna:
+        datos_participante (list): datos del participante filtrado
+
+    '''
+    datos_participante = []
+    for registro in datos:     
+       if id_participante == registro["id_participante"]:
+            datos_participante.append(registro)
+    return datos_participante
