d93cd42fb0d449e8a423594e76e91f61c7b54521
Maria Emilia Barbeito <mariaemiliabarbeito@MacBook-Air-98.local>
2026-04-19 23:35:08 -0300

    .

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -6,4 +6,22 @@ Created on Tue Apr 14 10:38:23 2026
 @author: mariaemiliabarbeito
 """
 
-validacion_datos
+def validar_registro (registro):
+    for datos in registro:
+        try:
+            id_participante = int(datos["id_participante"]): 
+        except:
+          raise TypeError("ID invalido")
+        if id_participante <0:
+          raise ValueError ("ID invalido")
+              
+        
+            
+  
+            
+      
+            
+            
+                
+                
+            
