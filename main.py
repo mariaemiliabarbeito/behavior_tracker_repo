c565ea46e6e5c3eccc1ee04c98073a61c3bd550c
matilde-urrestarazu <murrestarazuromero@udesa.edu.ar>
2026-04-22 10:52:59 -0300

    hice main

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ Created on Tue Apr 14 10:40:35 2026
 """
 
 from src.carga_datos import cargar_datos
-<<<<<<< HEAD
-from src.validacion_datos import validar_registro
-from src.metricas import (calcular_tiempo_total, calcular_promedio_uso, calcular_uso_por_app)
-=======
-from src.validacion_datos import validar_datos
-from src.metricas import calcular_tiempo_total, calcular_promedio_uso
->>>>>>> 24b270ee1f965bc154baa4701a7031d5862f581d
+## no importo validar_datos porque se importa dentro de cargar_datos
 from src.procesamiento_datos import filtrar_por_participante
+from src.metricas import (calcular_tiempo_total, calcular_promedio_uso, calcular_uso_por_app)
+
+
 try:
     datos = cargar_datos ("datos/datos_proyecto.csv")
 except FileNotFoundError as e:
@@ -26,11 +23,11 @@ except ValueError as e:
 except Exception as e:
    print (f"Error inesperado: {e}")
    exit()
+   
 
-<<<<<<< HEAD
 try:
     id_participante = input("ingrese el id del participante que desea filtrar: ")
-    datos_filtrados = [filtrar_por_participante(datos, id_participante)]
+    registro_participante = [filtrar_por_participante(datos, id_participante)] ##lo pongo dentro de una lista para poder usarlo en metricas
 except ValueError as e:
     print(f"Error con el id o con el participante: {e} ")
     exit()
@@ -40,24 +37,22 @@ except KeyError as e:
 except Exception as e:
    print (f"Error inesperado: {e}")
    exit()
-    
-##%%%%%%%%la parte de metricas nose como hacerla
-datos_validos= []
-for registro in datos: 
-    if validar_registro(registro):
-=======
-def main ():
-    datos = cargar_datos("datos/datos_proyecto.csv")
-    datos_validos= []
+   
+###metricas
+try:
+    tiempo_total= calcular_tiempo_total(registro_participante)
+    promedio_uso= calcular_promedio_uso (registro_participante)
+    uso_por_app = calcular_uso_por_app(registro_participante)
+except ZeroDivisionError as e: 
+    print(f"Error cuando se calcula el promedio: {e}")
+except KeyError as e:
+     print(f" error con datos: {e}")
+     exit()
+except Exception as e:
+    print (f"Error inesperado: {e}")
+    exit()
 
-    for registro in datos: 
-    if validar_datos(registro):
->>>>>>> 24b270ee1f965bc154baa4701a7031d5862f581d
-        datos_validos.append(registro)
-id_participante= int(input("Ingrese id del participante: "))
-datos_participante = filtrar_por_participante(datos_validos,id_participante)
-tiempo_total= calcular_tiempo_total(datos_validos)
-promedio_uso= calcular_promedio_uso (datos_validos)
-
-print("Tiempo total: {tiempo_total}")
-print("El promedio de uso es: {promedio_uso")
+print(f"Los resultados para el participante {id_participante} son: ")
+print("El Tiempo total fue: {tiempo_total}")
+print("El promedio de uso fue: {promedio_uso}")
+print(f"el uso por app fue: {uso_por_app}")
