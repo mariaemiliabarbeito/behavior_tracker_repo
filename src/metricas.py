6119a6e30020f2624d4e4226c77a908fe1c38910
Maria Emilia Barbeito <mariaemiliabarbeito@MacBook-Air-98.local>
2026-05-28 00:51:11 -0300

    metricas y validacion

--- src/metricas.py
+++ src/metricas.py
@@ -6,36 +6,32 @@ Created on Tue Apr 14 10:39:37 2026
 @author: mariaemiliabarbeito
 """
 
-def calcular_tiempo_total(datos):
+def calcular_tiempo_total(df):
     """
     Que hace la funcion: calcula el tiempo total de uso de las aplicaciones
     
     Parameters
     ----------
-    datos : lista
-         lista, donde cada elemento es un diccionario que contiene un participante
+    df: Dataframe
+         Dataframe validado 
 
     Returns  
     -------
-    type :  float
-           teimpo total en el que se usaron todas las aplicaciones
+    tiempo_total :  int
+           suma tiempo total en el que se usaron todas las aplicaciones
 
     """
-    tiempo_total=0
-    for participante in datos: 
-       tiempos= participante["tiempo_uso"]
-       tiempo_total += sum(tiempos)
+    tiempo_total= df["tiempo_uso"].sum()
     return tiempo_total
 
-def calcular_promedio_uso(datos): 
+def calcular_promedio_uso(df): 
     """
     Que hace: calcula el tiempo promedio en el que se usan las aplicaciones.
 
     Parametros: 
     ----------
-    datos : lista
-        lista, donde cada elemento es un diccionario que contiene un participante
-
+    df:dataframe
+        Dataframe validado 
     Returns
     -------
     promedio : float
@@ -45,44 +41,29 @@ def calcular_promedio_uso(datos):
         si los usos totales son iguales a 0
 
     """
-    tiempo_total= calcular_tiempo_total(datos)
-    
-    total_usos = 0
-    for participante in datos:
-        total_usos += sum(participante["cantidad_uso"])
-    
-    if total_usos == 0:
-        raise ZeroDivisionError ("No se puede calcular el promedio")
-    promedio= tiempo_total / total_usos
-    
-    return promedio
+    if df["cantidad_uso"].sum()==0:
+        raise ZeroDivisionError ("No hay datos, no se puede calcular el promedio")
+    promedio_uso= df["cantidad_uso"].mean()
+    return promedio_uso
 
-def calcular_uso_por_app(datos):
+
+def calcular_uso_por_app(df):
     """
     Que hace la funcion: calcula cuantas veces se utilizo cada app.
 
     Parametro
     ----------
-    datos : lista
-        lista , donde cada elemento es un diccionario que contiene un  participante
+    df: dataframe
+        Dataframe validado 
 
     Returns
     -------
-    TYPE: dict
-        Diccionario con la cantidad de usos por aplicación. La clave es la app, el valor la cantidad de usos
+    uso_por_app: series
+             Uso por app
     """
-    uso_por_app={}
-    for participante in datos:
-        apps= participante["app"]
-        usos= participante["cantidad_uso"]
-        for i in range(len(apps)):
-            app = apps [i]
-            uso=usos [i]
-            if app in uso_por_app:        
-               uso_por_app[app]  += uso 
-            else:
-               uso_por_app [app] =uso
+    uso_por_app = df.groupby("app")["cantidad_uso"].sum()
     return uso_por_app
+    
             
     
     
